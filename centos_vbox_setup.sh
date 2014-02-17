611a1648e2aafd87381361383f2ca490eadd42ba
Mike Holzinger <mike.holzinger@gmail.com>
2014-02-16 22:05:44 -0800

    Added rpmforge repository for HTOP

--- centos_vbox_setup.sh
+++ centos_vbox_setup.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
+mkdir ~/bin
+cat>~/bin/amiup<<'EOF'
+#!/bin/bash
+
+ping_internets() {
+	ping -c 2 google.com > /dev/null 2>&1
+
+	check_err "Nope - google.com is not reachable. No internets for you :["
+
+	# if we have a 0 exit status for ping we report this
+	echo "Yay! google.com is reachable. You're online!"
+}
+
+check_err(){ error_state=$(echo $?)
+if [[ "$error_state" != "0" ]];then
+    echo $1
+    exit
+fi
+}
+
+ping_internets
+EOF
+
+chmod a+x ~/bin/amiup
+
 # User specific aliases and functions
 echo "PS1='\[\033[02;32m\]\u@\H:\[\033[02;34m\]\w\$\[\033[00m\] '" >> ~/.bashrc
 echo "export PATH="$HOME/bin:$PATH"" >> ~/.bashrc
-mkdir ~/bin
 
 # Add current user to /etc/sudoers
 currentuser=$( whoami )
 su -c "echo '$currentuser  ALL=(ALL:ALL) ALL' >> /etc/sudoers"
 
-# setup python dependencies
+# Setup python dependencies
 sudo easy_install pexpect
 
-# use python to generate ssh keys
+# Use python pexpect to generate ssh keys
 python - <<END
 #!/usr/bin/python
  
@@ -38,15 +62,16 @@ sudo chkconfig NetworkManager off
 # Add default Net Manager
 sudo chkconfig network on
 
-# Sudo yum -y uninstall NetworkManager
 sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
 
 system-config-network
 service network restart
 
-# Install htop
+# Install rpmforge packages
 cd /tmp
-wget http://apt.sw.be/redhat/el6/en/x86_64/rpmforge/RPMS/htop-0.9-1.el6.rf.x86_64.rpm
-sudo rpm -i htop-0.9-1.el6.rf.x86_64.rpm
+wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el5.rf.x86_64.rpm
+sudo rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el5.rf.x86_64.rpm
 
+# Install HTOP
+sudo yum -y install htop
 
