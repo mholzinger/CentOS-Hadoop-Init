7d0cb59e61ca7febc590ebe3d37e9ff0568d5dba
Mike Holzinger <mike.holzinger@gmail.com>
2014-04-08 15:18:59 -0700

    Install git on RPM system

--- install_git.sh
+++ install_git.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# TODO: Use curl to grab the latest version string for git
+
+# current version 1.9.1
+GIT_MAJOR=1.9
+GIT_MINOR=1
+GIT_VER=$GIT_MAJOR.$GIT_MINOR
+
+# TODO: Run as root
+
 yum install gettext-devel expat-devel curl-devel zlib-devel openssl-devel
 cd /usr/local/src
-wget https://www.kernel.org/pub/software/scm/git/git-1.8.2.3.tar.gz
-tar xzvf git-1.8.2.3.tar.gz
-cd git-1.8.2.3
+wget https://www.kernel.org/pub/software/scm/git/git-$GIT_VER.tar.gz
+tar xzvf git-$GIT_VER.tar.gz
+cd git-$GIT_VER
 make prefix=/usr/local all
 make prefix=/usr/local install
+
